4c076f7846fe878e7ad81f0d5044db6d69fba28d
Rota <sryota461@gmail.com>
2023-11-26 12:03:58 +0900

    change tab stop 4 to 2

--- lua/options.lua
+++ lua/options.lua
@@ -1,8 +1,8 @@
 vim.opt.encoding = 'utf-8'
 vim.scriptencoding = 'utf-8'
 
-vim.o.tabstop = 4
-vim.o.shiftwidth = 0
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
