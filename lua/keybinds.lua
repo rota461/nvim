42872c04ca53280035a782c640ede824ae810faf
Rota <sryota461@gmail.com>
2023-12-04 07:18:14 +0900

    update treesitter config

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -4,3 +4,4 @@ local bind = vim.keymap.set
 
 bind('n', '<leader>e', '<cmd>NvimTreeToggle<cr>')
 bind('n', '<C-p>', '<cmd>Telescope find_files<cr>')
+bind('n', '<C-g>', '<cmd>Telescope live_grep<cr>')
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,14 @@
 return {
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {"nvim-treesitter/nvim-treesitter", 
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = {"c", "lua", "vim", "vimdoc", "tsx", "javascript", "typescript", "python"},
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  },
 }
